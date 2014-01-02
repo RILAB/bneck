f44907978dca2fe2cc9272b62751219dcde6c5f2
rossibarra <rossibarra@ucdavis.edu>
2014-01-01 22:24:29 -0800

    first sims -- constant size model two pops

--- bneck.sh
+++ bneck.sh
@@ -4,22 +4,22 @@
 #SBATCH -o outs/out-%j.txt
 #SBATCH -p bigmem
 #SBATCH -e errors/error-%j.txt
-#SBATCH -c 8
 
+module load libsequence
 
 N=100000 # wild Ne
 t_recover=1000 # generations since recovery
-mu=$(echo "3*10 ^ -8" | bc -l)
-region=100000
-theta=$( echo "4*$N*$mu*$region" | bc -l )
-bneck_N=23200 #Ne during bneck
+mu=$(echo "3*10 ^ -8" | bc -l) # mutation rate
+region=100000 # length of region
+theta=$( echo "4*$N*$mu*$region" | bc -l ) # assume rho = theta
+bneck_N=23200 # Ne during bneck
 bneck_dur=5000 # generations
-alpha=$( echo "-(1/(($bneck_dur)/(4*$N)))*l($bneck_N/$N)" | bc -l )
+alpha=$( echo "-(1/(($bneck_dur)/(4*$N)))*l($bneck_N/$N)" | bc -l ) # growth rate
 
 seed="$RANDOM $RANDOM $RANDOM"
 
-echo "ms 10 1000 -t $theta -r $theta $region -eN $( echo  "$t_recover/(4*$N)" | bc -l ) $( echo "$bneck_N/$N" | bc -l ) -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | samplestats | cut -f 2 | /home/jri/src/msdir/stats" 
-ms 10 1000 -t $theta -eN $( echo  "$t_recover/(4*$N)" | bc -l ) $( echo "$bneck_N/$N" | bc -l ) -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats
+echo "ms 20 1000 -t $theta -r $theta $region -I 2 10 10 -n 2 $( echo "$N - $bneck_N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed " 1>&2
+ms 20 1000 -t $theta -r $theta $region -I 2 10 10 -n 2 $( echo "$N - $bneck_N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats 
 
 #straight growth
 #echo "ms 10 1000 -t $theta -eG $( echo  "$t_recover/(4*$N)" | bc -l ) $alpha -eG $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 0 -seed $seed | samplestats "
