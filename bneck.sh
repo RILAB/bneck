25c409d2b5f99778e0969bc539919dd149aa4573
rossibarra <rossibarra@ucdavis.edu>
2014-01-02 20:42:41 -0800

    new models w/ two pops, uses msstats with fst, also multiple cpu

--- bneck.sh
+++ bneck.sh
@@ -1,26 +1,32 @@
 #!/bin/bash -l
 #SBATCH -D /home/jri/projects/bneck
 #SBATCH -J bneck
-#SBATCH -o outs/out-%j.txt
-#SBATCH -p bigmem
-#SBATCH -e errors/error-%j.txt
+#SBATCH -o outs/out-%A.%a.txt
+#SBATCH -p serial
+#SBATCH --array=1-100
+#SBATCH -e errors/error-%A.%a.txt
 
 module load libsequence
 
 N=100000 # wild Ne
-t_recover=1000 # generations since recovery
+t_recover=5000 # generations since recovery
 mu=$(echo "3*10 ^ -8" | bc -l) # mutation rate
 region=100000 # length of region
 theta=$( echo "4*$N*$mu*$region" | bc -l ) # assume rho = theta
-bneck_N=23200 # Ne during bneck
-bneck_dur=5000 # generations
+bneck_N=4650 # Ne during bneck
+bneck_dur=1000 # generations
 alpha=$( echo "-(1/(($bneck_dur)/(4*$N)))*l($bneck_N/$N)" | bc -l ) # growth rate
+n1=100 # sampled chroms from each pop
+n2=100
 
 seed="$RANDOM $RANDOM $RANDOM"
 
-echo "ms 20 1000 -t $theta -r $theta $region -I 2 10 10 -n 2 $( echo "$N - $bneck_N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed " 1>&2
-ms 20 1000 -t $theta -r $theta $region -I 2 10 10 -n 2 $( echo "$N - $bneck_N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats 
+echo "N t_recover mu region theta bneck_N bneck_dur alpha n1 n2" 1>&2
+echo "$N $t_recover $mu $region $theta $bneck_N $bneck_dur $alpha $n1 $n2" 1>&2
 
-#straight growth
-#echo "ms 10 1000 -t $theta -eG $( echo  "$t_recover/(4*$N)" | bc -l ) $alpha -eG $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 0 -seed $seed | samplestats "
-#ms 10 1000 -t $theta -eG $( echo  "$t_recover/(4*$N)" | bc -l ) $alpha -eG $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 0 -seed $seed | samplestats | cut -f 10 | /home/jri/src/msdir/stats
+#3 epoch model
+#echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed " 1>&2
+#ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats -I 2 $n1 $n2 -F
+
+#exponential growth
+echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -eg $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $alpha -eg $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 0 -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed" 1>&2 
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Bneck sims
+
+Do some bneck sims to show similarity and differences of bneck models.
+
+See [r code](http://rpubs.com/rossibarra/bneck) for descriptions of models.
+
+Currently using the 
