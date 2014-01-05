1d3e8b52e6a481b173516416c9743006a8898630
rossibarra <rossibarra@ucdavis.edu>
2014-01-04 18:38:01 -0800

    additional runs

--- bneck.sh
+++ bneck.sh
@@ -13,8 +13,8 @@ t_recover=1 # generations since recovery
 mu=$(echo "3*10 ^ -8" | bc -l) # mutation rate
 region=100000 # length of region
 theta=$( echo "4*$N*$mu*$region" | bc -l ) # assume rho = theta
-bneck_N=23250 # Ne during bneck
-bneck_dur=5000 # generations
+bneck_N=10500 # Ne during bneck
+bneck_dur=6000 # generations
 alpha=$( echo "-(1/(($bneck_dur)/(4*$N)))*l($bneck_N/$N)" | bc -l ) # growth rate
 n1=100 # sampled chroms from each pop
 n2=100
@@ -25,9 +25,10 @@ echo "N t_recover mu region theta bneck_N bneck_dur alpha n1 n2" 1>&2
 echo "$N $t_recover $mu $region $theta $bneck_N $bneck_dur $alpha $n1 $n2" 1>&2
 
 #3 epoch model
-echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed " 1>&2
-ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats -I 2 $n1 $n2 -F
+#echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed " 1>&2
+#ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -en $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $( echo "$bneck_N/$N" | bc -l ) -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats -I 2 $n1 $n2 -F
 
 #exponential growth
-#echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -eg $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $alpha -eg $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 0 -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed" 1>&2 
-#ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -eg $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $alpha -eg $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 0 -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats -I 2 $n1 $n2 -F
+echo "ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -eg $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $alpha -eg $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 0 -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed" 1>&2 
+ms $(( $n1 + $n2 )) 10 -t $theta -r $theta $region -I 2 $n1 $n2 -n 2 $( echo "($N - $bneck_N)/$N" | bc -l ) -eg $( echo  "$t_recover/(4*$N)" | bc -l ) 1 $alpha -eg $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 0 -ej $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 2 -eN $( echo "($t_recover+$bneck_dur)/(4*$N)" | bc -l ) 1 -seed $seed | msstats -I 2 $n1 $n2 -F
+
